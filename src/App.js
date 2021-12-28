16a21766b8772cd25ffe8fb1053808497c12d739
luigi <luigilamastra@gmail.com>
2021-12-28 07:14:59 +0100

    implemented counter with reducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import { UserContext } from './index'
-import { useContext } from 'react';
+import { useContext, useReducer } from 'react';
+
+const initialState = {
+  count: 0
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment": return {count: state.count + 1}
+    case "decrement": return {count: state.count - 1}
+    case "reset": return initialState
+      
+  
+    default: return initialState
+  }
+}
 function App() {
-  const value = useContext(UserContext)
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
-    <div >
-      Hello {value}
+    <div className="p-2 ">
+      {state.count}
+
+  <button className="border p-1 m-1"
+  onClick={()=>dispatch({type:"increment"})}
+  > increment</button>
+  <button className="border p-1 m-1"
+  onClick={()=>dispatch({type:"decrement"})}
+  > decrement</button>
+  <button className="border p-1 m-1"
+  onClick={()=>dispatch({type:"reset"})}
+  > reset</button>
+
     </div>
   );
 }
